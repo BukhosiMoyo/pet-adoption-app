86903ce51315c05148d187266b640ac5d0989f29
Bukhosi Moyo <Bukhosizimcode@gmail.com>
2020-08-25 10:44:30 +0200

    Added now files to the projects after adding models

---
+++ adoptions/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Pet(models.Model):
+
+    SEX_CHOICES = [
+        ("M", "Male"), ("F", "Female")
+    ]
+
+    name = models.CharField(max_length=100)
+    subitter = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    sex = models.CharField(max_length=1, blank=True, choices=SEX_CHOICES)
+    submission_date = models.DateTimeField()
+    age = models.IntegerField(null=True)
+    vaccinations = models.ManyToManyField("Vaccine", blank=True)
+
+class Vaccine(models.Model):
+
+    name = models.CharField(max_length=50) 
